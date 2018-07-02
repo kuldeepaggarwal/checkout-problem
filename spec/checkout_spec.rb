a7c17aafa2d78f449b30a52b42c890a638f784f5
Kuldeep Aggarwal <kd.engineer@yahoo.co.in>
2018-07-02 15:45:42 +0530

    Create more sample products and update the specs

--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -2,24 +2,26 @@ require 'spec_helper'
 
 describe Checkout do
   before do
-    Product.create(code: 'A201', price: 23, currency: 'USD')
+    Product.create(code: '001', price: 1.95, currency: 'USD')
+    Product.create(code: '002', price: 5.99, currency: 'USD')
+    Product.create(code: '003', price: 25.00, currency: 'USD')
   end
 
   describe '#scan' do
     it 'scans the item in the checkout' do
-      expect { subject.scan('A201') }.to change(subject, :item_count).by(1)
-      expect { subject.scan('A201') }.not_to change(subject, :item_count)
+      expect { subject.scan('001') }.to change(subject, :item_count).by(1)
+      expect { subject.scan('001') }.not_to change(subject, :item_count)
     end
   end
 
   describe '#total' do
     before do
-      subject.scan('A201')
-      subject.scan('A201')
+      subject.scan('003')
+      subject.scan('003')
     end
 
     it 'returns the total amount to be paid' do
-      expect(subject.total).to eq(46)
+      expect(subject.total).to eq(50)
     end
   end
 end
